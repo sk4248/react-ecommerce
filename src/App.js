34e697136cb6eaac49328eae65329ae84622723c
sreekanth korampally <sreekanthkorampally@sreekanths-MBP.attlocal.net>
2020-03-30 21:02:34 -0500

    broken the homepage into different components and finished styling the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
-    <div >
-      Hello World!
+    <div>
     <HomePage/>
     </div>
   );
